858578cca50bbcf95be728665fb5b72cfd668b41
Your Name <nicola09763573@gmail.com>
2026-01-18 21:20:09 +0300

    feat: Добавил в настройки бинды под клавиши по умолчанию

--- code/settings.py
+++ code/settings.py
@@ -1,5 +1,17 @@
+import arcade
+
 SCREEN_TITLE = 'Time Control Platformer'
 
 # Пока None
 PLAYER_SPEED = None
 COOLDOWN_DURATION = None
+
+KEYBINDS = {
+    "move_left": arcade.key.A,
+    "move_right": arcade.key.D,
+    "jump": [arcade.key.W, arcade.key.SPACE],
+    "fast_fall": arcade.key.S,
+    "time_stop": arcade.key.T,
+    "rewind": arcade.key.R,
+    "select_object": arcade.MOUSE_BUTTON_LEFT
+}
